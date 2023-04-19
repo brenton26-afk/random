30fe26cda2afab49f3615c59296e09bbb71524c7
brenton26-afk <77509555+brenton26-afk@users.noreply.github.com>
2023-04-19 13:22:13 -0400

    Update EchoCV.py

--- Robotics_stuff/EchoCV.py
+++ Robotics_stuff/EchoCV.py
@@ -1,2 +1,30 @@
 # project echo. Code to get a camera to recognize things. 
 # Goal: recognize reflective tape on dark cement.
+
+# import the cv library
+import cv2
+
+# video capture object
+vid = cv2.VideoCapture(0)
+
+# this should always be running while being tested and used to see what the camera is seeing.
+while(True):
+      
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+  
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+      
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
+
