426ba3c00e23e7529f975fd99b0835701400e43d
brenton26-afk <77509555+brenton26-afk@users.noreply.github.com>
2023-04-26 12:40:25 -0400

    Update mainEcho.py

--- Elijah/mainEcho.py
+++ Elijah/mainEcho.py
@@ -1,15 +1,30 @@
 import cv2
 import numpy as np
-print("hello world")
+import math
+print("Hello World!")
 
 #find average light level
 # when the average light level is found "map" it into another window that will find and detect lines
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 #finds average light level
 def average_light(frame):
     return np.mean(gray_frame)
+
+
+def process_frame(frame):
+    blurred_frame = cv2.GaussianBlur(gray_frame, (5,5), 0)
+
+    _, thresholded_frame = cv2.threshold(blurred_frame, 200, 255, cv2.THRESH_BINARY)
+    lines = cv2.HoughLinesP(thresholded_frame, 1, np.pi / 180, 100, minLineLength=50, maxLineGap=10)
+
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    
+    return frame
 
 
 while True:
@@ -26,10 +41,17 @@ while True:
     averageLevel = average_light(frame)
     printLevel = str(averageLevel)
     print("Average Light Level is: " + printLevel)
+
+    #finding white line function
+    processed_frame = process_frame(frame)
     
     #Video windows
-    cv2.imshow('BandW', img_inv)
+    #cv2.imshow('BandW', img_inv)
+    #show just lines
+    #cv2,imshow('just lines', )
+    
     cv2.imshow('OG Video', frame)
+    cv2.imshow('other Window', processed_frame)
     
     
     if cv2.waitKey(10) & 0xFF == ord('q'):
