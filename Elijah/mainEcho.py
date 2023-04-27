9551bfb9dfab5cc67d41efe65113f8182e793844
brenton26-afk <77509555+brenton26-afk@users.noreply.github.com>
2023-04-26 22:15:07 -0400

    Update mainEcho.py

--- Elijah/mainEcho.py
+++ Elijah/mainEcho.py
@@ -6,11 +6,11 @@ print("Hello World!")
 #find average light level
 # when the average light level is found "map" it into another window that will find and detect lines
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 #finds average light level
 def average_light(frame):
-    return np.mean(gray_frame)
+    return np.mean(frame)
 
 
 def process_frame(frame):
@@ -23,6 +23,8 @@ def process_frame(frame):
         for line in lines:
             x1, y1, x2, y2 = line[0]
             cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            if (x2 >= 320) & (y1 > y2) & (x1 < 320) & (x2 > x1):
+                print("Right")
     
     return frame
 
@@ -37,24 +39,21 @@ while True:
     
     #invert B&W because i want to
     img_inv = cv2.flip(blackAndWhiteFrame, 1)
-    
+
     # Vertical direction line: "arrow"
-    '''
+    #>
     height, width, _ = frame.shape
-    black_frame = np.zeros((height, width, 3), dtype=npuint8)
-    
-    middle = int(width/2)
-    
-    
-    cv2.line(black_frame, )
-    '''
+    black_frame = np.zeros((height, width, 3), dtype=np.uint8)
+    middle = int(width/2)    
+    cv2.line(black_frame, (320, 480), (middle, 180), (255, 0, 0), 5)
+    # ^
     
     averageLevel = average_light(frame)
     printLevel = str(averageLevel)
-    print("Average Light Level is: " + printLevel)
+    #print("Average Light Level is: " + printLevel)    
 
     #finding white line function
-    processed_frame = process_frame(frame)
+    processed_frame = process_frame(black_frame)
     
     #Video windows
     #cv2.imshow('BandW', img_inv)
@@ -62,10 +61,10 @@ while True:
     # A video window that only shows the green lines. Then add a single "arrow" line in the middle pointing forward.
     # Find if the "arrow" intersects a green line for a couple seconds then figure how to change course.
     # Could use angles and math. or map cords ==> if when x1 is smaller than x2 y1 will be higher than y2 straying right.
-    #cv2,imshow('just lines', )
+    cv2.imshow('just lines', black_frame)
     
-    cv2.imshow('OG Video', frame)
-    cv2.imshow('other Window', processed_frame)
+    #cv2.imshow('OG Video', frame)
+    cv2.imshow('other Window', frame)
     
     
     if cv2.waitKey(10) & 0xFF == ord('q'):
