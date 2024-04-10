7e7ed898c7aa63f48288b76e27e40ba92fbab73c
brenton26-afk <77509555+brenton26-afk@users.noreply.github.com>
2024-04-09 22:26:56 -0400

    Update Arduino_Test.ino

--- Arduino_Test.ino
+++ Arduino_Test.ino
@@ -1,5 +1,5 @@
-// This script is a simple script to test if Serial port is working 
-// There is also simple scrip to test the power and output of a board by setting up a simpling blinking LED
+// This script is a simple script to test if the Serial port is working 
+// There is also a simple script to test the power and output of a board by setting up a simple blinking LED
 
 void setup(){
   // Set up Serial Port
@@ -7,15 +7,22 @@ void setup(){
   Serial.print("Hello World");
 
   //Set up a pin
-  // For example, for a LED
+  // For example, for an LED
   pinMode(10, OUTPUT);
 }
 
 // The looping function that will continuously run.
 void loop(){
-  // Turning on and off a LED.
+  // Turning on and off an LED.
   digitalWrite(10, HIGH);
   delay(1000);
   digitalWrite(10, LOW);
   delay(1000);
 }
+
+
+
+// Process of wiring a single LED to an Arduino and breadboard:
+// 5v and GND must be wired correctly. Also, there must be a ground wire connecting both GNDs of the breadboard.
+// Long side of the LED goes on the inside of the breadboard along with a resistor.
+// There is also a wire to the resistor and towards a pin in the Arduino.
